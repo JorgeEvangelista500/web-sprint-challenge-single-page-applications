b979d207ef81709314fdbd5e4e05beea97339173
Jorge Evangelista <jorgeevangelista500@gmail.com>
2021-12-17 11:29:59 -0500

    implemented change and submit in my pizzaform

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect} from "react";
 import {Route, Link,} from 'react-router-dom'
 import axios from "axios";
+import * as yup from 'yup';
 
 const initialFormValues = {
   name: '',
@@ -11,26 +12,46 @@ const initialFormValues = {
   canadian_bacon: false,
   spicy_italian_sausage: false,
   special: '',
-
 }
+const initialFormErrors = {
+  name: '',
+  size: '',
+  sauce: '',
+  
+}
+const initialOrders = []
+const initialDisabled = true
 
 const App = () => {
-  const [order, setOrder] =[]
-  const [formValues, setFormValues] = useState(initialFormValues)
 
+  const [orders, setOrder] = (initialOrders)
+  const [formValues, setFormValues] = useState(initialFormValues)
+  const [formErrors, setFormErrors] = useState(initialFormErrors)
+  const [disabled, setDisable] = useState(initialDisabled)
+
+
+  const postNewOrder = newOrder => {
+    axios.post('https://reqres.in/api/orders', newOrder)
+      .then(resp => {
+        setOrder([ resp.data, ...orders])
+      }).catch(err => console.error(err))
+      .finally(() => setFormValues(initialFormValues))
+  }
   const inputChange = (evt) => {
     setFormValues({ ...formValues,[evt.target.name]: evt.target.value})
   }
 
-  const submit = (evt) => {
-    evt.preventDefault();
-    setFormValues
+  const formSubmit = (evt) => {
+    const newOrder = {
+      name: formValues.name.trim(),
+      size: formValues.size,
+      sauce: formValues.sauce,
+      toppings:['pepperoni', 'sausage', 'canadian_bacon', 'spicy_italian_sausage'].filter(topping => !!formValues[topping]),
+      special: formValues.special
+    }
+    postNewOrder(newOrder)
   }
   
-
-
-
-
 
   return (
     <>

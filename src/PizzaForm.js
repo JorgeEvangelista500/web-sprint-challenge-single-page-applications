c82554f397ad1f2aea813dc953795da4cbc2e98b
Jorge Evangelista <jorgeevangelista500@gmail.com>
2021-12-17 10:02:24 -0500

    set up app.js and index.js

---
+++ src/PizzaForm.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const PizzaForm = (props) => {
+    return(
+        <form id="pizza-form">
+            <label>Pizza size
+                <select> 
+                    <option value=''>- Select an option -</option>
+                    <option value='Small'>Small</option>
+                    <option value='Medium'>Medium</option>
+                    <option value='Large'>Large</option>
+                    <option value='Extra Large'>Extra Large</option>
+                </select>
+            </label>
+            <label>
+                <input
+                    type='radio'
+                >
+            
+
+
+
+
+            </label>
+        </form>
+    )
+}
